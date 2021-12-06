0f6c9ced907b6761d6b49634c7b3b39adfba8177
merelinguist <24858006+merelinguist@users.noreply.github.com>
2021-12-06 13:58:40 +0000

    Remove dataproxy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,86 +1,26 @@
 import type { LoaderFunction } from "remix";
-import { json, Link, useLoaderData } from "remix";
+import { json, useLoaderData } from "remix";
+
+import { db } from "~/utils/db.server";
 
 type IndexData = {
 	resources: { name: string; url: string }[];
 	demos: { name: string; to: string }[];
 };
 
-export const loader: LoaderFunction = () => {
-	const data: IndexData = {
-		resources: [
-			{
-				name: "Remix Docs",
-				url: "https://remix.run/docs",
-			},
-			{
-				name: "React Router Docs",
-				url: "https://reactrouter.com/docs",
-			},
-			{
-				name: "Remix Discord",
-				url: "https://discord.gg/VBePs6d",
-			},
-		],
-		demos: [
-			{
-				to: "demos/actions",
-				name: "Actions",
-			},
-			{
-				to: "demos/about",
-				name: "Nested Routes, CSS loading/unloading",
-			},
-			{
-				to: "demos/params",
-				name: "URL Params and Error Boundaries",
-			},
-		],
-	};
+export const loader: LoaderFunction = async () => {
+	const decks = await db.deck.findMany();
 
-	return json(data);
+	return json(decks);
 };
 
 export default function IndexRoute() {
-	const data = useLoaderData<IndexData>();
+	const decks = useLoaderData<IndexData>();
 
 	return (
 		<>
-			<main>
-				<h1>Welcome to Remix!</h1>
-				<p>We’re stoked that you’re here. 🥳</p>
-				<p>
-					Feel free to take a look around the code to see how Remix does things,
-					it might be a bit different than what you’re used to. When you’re
-					ready to dive deeper, we’ve got plenty of resources to get you
-					up-and-running quickly.
-				</p>
-				<p>
-					Check out all the demos in this starter, and then just delete the{" "}
-					<code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
-					folders when you’re ready to turn this into your next project.
-				</p>
-			</main>
-			<aside>
-				<h2>Demos In This App</h2>
-				<ul>
-					{data.demos.map((demo) => (
-						<li key={demo.to}>
-							<Link prefetch="intent" to={demo.to}>
-								{demo.name}
-							</Link>
-						</li>
-					))}
-				</ul>
-				<h2>Resources</h2>
-				<ul>
-					{data.resources.map((resource) => (
-						<li key={resource.url}>
-							<a href={resource.url}>{resource.name}</a>
-						</li>
-					))}
-				</ul>
-			</aside>
+			<h1>Welcome to Strove!</h1>
+			<pre>{JSON.stringify(decks, null, 2)}</pre>
 		</>
 	);
 }
