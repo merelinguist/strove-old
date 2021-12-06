99b8779e4232fcae2c1011fadee7a83d7be5996b
merelinguist <24858006+merelinguist@users.noreply.github.com>
2021-12-06 13:06:53 +0000

    Use postgres

--- prisma/migrations/20211203181551_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Log" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
--- prisma/migrations/20211203212615_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Response" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "correctness" REAL NOT NULL,
-    "cardId" TEXT NOT NULL,
-    CONSTRAINT "Response_cardId_fkey" FOREIGN KEY ("cardId") REFERENCES "Card" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
